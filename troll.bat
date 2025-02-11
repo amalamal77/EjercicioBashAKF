25f05d41058bf6adf833483e046fe399cca0d52a
amalamal77 <amalkf2003@gmail.com>
2025-02-11 14:31:21 +0100

    ventana emergenet

--- troll.bat
+++ troll.bat
@@ -1,3 +1,10 @@
 @echo off
 
 for /l %%i in (1,1,5) do start chrome "https://www.google.com"
+
+:: Broma 4: Hacer que el cursor se mueva por la pantalla
+echo Moviendo el cursor por toda la pantalla...
+for /l %%i in (1,1,20) do (
+    echo.  %%i
+    timeout /t 1 >nul
+)
