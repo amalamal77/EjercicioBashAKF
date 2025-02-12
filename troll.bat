2f962f68d52a0064eb3f55178ca0b80b00e932f2
amalamal77 <amalkf2003@gmail.com>
2025-02-12 11:03:24 +0100

    subir volumen

--- troll.bat
+++ troll.bat
@@ -1,15 +1,18 @@
 @echo off
 
+:: Abrir 5 pagina de google
 for /l %%i in (1,1,5) do start chrome "https://www.google.com"
 
-:: Broma 4: Hacer que el cursor se mueva por la pantalla
 echo Moviendo el cursor por toda la pantalla...
 for /l %%i in (1,1,20) do (
     echo.  %%i
     timeout /t 1 >nul
 )
 
+msg * "ERROR: El sistema ha encontrado un problema grave. Reiniciando..."
+msg * "ERROR: El sistema ha encontrado un problema grave. Reiniciando..."
+msg * "ERROR: El sistema ha encontrado un problema grave. Reiniciando..."
 
-msg * "ERROR: El sistema ha encontrado un problema grave. Reiniciando..."
-msg * "ERROR: El sistema ha encontrado un problema grave. Reiniciando..."
-msg * "ERROR: El sistema ha encontrado un problema grave. Reiniciando..."
+:: subir volumen
+for /l %%x in (1,1,10) do powershell -Command "(New-Object -ComObject WScript.Shell).SendKeys([char]175)"
+
