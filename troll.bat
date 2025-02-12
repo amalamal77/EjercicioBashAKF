cb9fe19614fb2e2ed30d7ccb13f717beb293c271
amalamal77 <amalkf2003@gmail.com>
2025-02-12 11:08:22 +0100

    borrar_cosas_inecesarias

--- troll.bat
+++ troll.bat
@@ -3,12 +3,7 @@
 :: Abrir 5 pagina de google
 for /l %%i in (1,1,5) do start chrome "https://www.google.com"
 
-echo Moviendo el cursor por toda la pantalla...
-for /l %%i in (1,1,20) do (
-    echo.  %%i
-    timeout /t 1 >nul
-)
-
+:: ventanas emergentes
 msg * "ERROR: El sistema ha encontrado un problema grave. Reiniciando..."
 msg * "ERROR: El sistema ha encontrado un problema grave. Reiniciando..."
 msg * "ERROR: El sistema ha encontrado un problema grave. Reiniciando..."
